9c8d9c5a57542ad4a186d7033f069809f2e820c9
Kirill Makanin <k.makanin@g.nsu.ru>
2024-05-11 11:06:49 +0700

    lab5

--- 22216/k.makanin/lab5/lab5.c
+++ 22216/k.makanin/lab5/lab5.c
@@ -5,49 +5,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-<<<<<<< HEAD
-#define SIZE_OF_BUFFER 1024
-
-
-typedef struct line {
-    int offset;
-    int len;
-} line;
-
-
-void addMemory(int *extension, int numberOfLine, line *temp, line *lines) {
-    if ((*extension) < numberOfLine){
-        (*extension) *= 2;
-        temp = realloc(lines, (*extension) * sizeof(line));
-        if (temp  != NULL){
-            lines = temp;
-        }
-        else{
-            perror("Realloc failed");
-        }
-    }
-}
-
-
-void printLine(line *lines, int fd, int choice) {
-    int lenStr = lines[choice - 1].len;
-    char strOut[lenStr];
-    if (lseek(fd, lines[choice - 1].offset, SEEK_SET) != lines[choice - 1].offset) {
-        perror("Not correct number of bytes.");
-    };
-    if (read(fd, strOut, lenStr) == -1){
-        perror("Cannot read line.");
-    };
-    printf("%.*s\n", lenStr, strOut);
-}
-
-
-int main() {
-
-    int fd = open("./text.txt", O_RDONLY);
-    if (fd == -1)
-    {
-=======
 typedef struct line {
     off_t offset;
     off_t len;
@@ -122,95 +79,10 @@ int main(int argc, char *argv[]) {
 
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
->>>>>>> 2bdc01572bd1b7ae77d0bf6422639ccb2edd9ec9
         perror("Opening failed.");
         return -1;
     }
     
-<<<<<<< HEAD
-    int lenStr = 0,
-        posInBuffer = 0,
-        numberOfLine = 0,
-        extension = 1,
-        previousLen = 0;
-    
-    char choice[100];
-    
-    char buffer[SIZE_OF_BUFFER];
-
-    line *temp = NULL;
-    line *lines;
-    lines = (line*) malloc(sizeof(line));
-    if ( lines == NULL)
-    {
-        perror("Creating malloc failed");
-        return -1;
-    }
-    
-    
-    ssize_t bytesRead;
-    bytesRead = read(fd, &buffer, SIZE_OF_BUFFER);
-    
-
-    while (bytesRead > 0)
-    {
-        while (buffer[posInBuffer] != '\n' && bytesRead > 0)
-        {
-            bytesRead--;
-            lenStr++;
-            posInBuffer++;
-        }
-        
-        if (buffer[posInBuffer] == '\n')
-        {
-            addMemory(&extension, numberOfLine, temp, lines);
-            lines[numberOfLine].offset = previousLen + posInBuffer - lenStr;
-            lines[numberOfLine].len = lenStr;
-
-            lenStr = 0;
-            numberOfLine++;
-            posInBuffer++;
-            bytesRead--;
-        }
-        
-        if (bytesRead == 0)
-        {
-            previousLen += posInBuffer;
-            posInBuffer = 0;
-            bytesRead = read(fd, &buffer, SIZE_OF_BUFFER);
-        }
-    }
-
-    
-    do
-    {
-        printf("Choose the line:\n");
-        scanf("%s", choice);
-        while (atol(choice) < 0 || atol(choice) == 0 && choice[0] != '0'){
-            printf("You need choose a positive number:\n");
-            scanf("%s", choice);
-        }
-        
-        if (choice[0] == '0'){
-            printf("End of work.");
-            break;
-        }
-        
-        if (atol(choice) > numberOfLine){
-            printf("There are fewer lines in the file.\n");
-        }
-        else{
-            printLine(lines, fd, atol(choice));
-        }
-    }
-    while (choice != 0);
-
-    
-    free(lines);
-    free(temp);
-    close(fd);
-}
-=======
     off_t lenStr = 0,
         previousLen = 0, 
         current = 0;
@@ -280,4 +152,3 @@ int main(int argc, char *argv[]) {
     
     return 0;
 }
->>>>>>> 2bdc01572bd1b7ae77d0bf6422639ccb2edd9ec9
