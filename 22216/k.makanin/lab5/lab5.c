4d72b55e6d86f2f41a5ada36d345e420680665de
Kirill Makanin <k.makanin@g.nsu.ru>
2024-05-17 11:49:01 +0700

    lab5

--- 22216/k.makanin/lab5/lab5.c
+++ 22216/k.makanin/lab5/lab5.c
@@ -34,46 +34,49 @@ int addMemory(int *extension, int numberOfLine, line **lines) {
 
 
 int printLine(line *lines, int fd, int choice) {
-    size_t lenStr = lines[choice - 1].len, specialOff = 0;
+    off_t lenStr = lines[choice - 1].len, specialOff = 0;
     char strOut[BUFSIZ];
     while (lenStr - specialOff > BUFSIZ){
         if (lseek(fd, lines[choice - 1].offset + specialOff, SEEK_SET) != lines[choice - 1].offset + specialOff) {
             perror("Not correct number of bytes.");
             return -1;
-        };
+        }
         if (read(fd, strOut, BUFSIZ) != BUFSIZ) {
             perror("Cannot read line.");
             return -1;
-        };
-        
-        strOut[lenStr] = '\0';
-        printf("%s", strOut);
+        }
+
+        if (fwrite(strOut, 1, BUFSIZ, stdout) != BUFSIZ){
+            perror("Cannot write line.");
+            return -1;
+        }
         specialOff += BUFSIZ;
     }
-    
+
     if (lenStr - specialOff > 0){
-        char lineOut[lenStr - specialOff + 1];
+        char lineOut[lenStr - specialOff];
         if (lseek(fd, lines[choice - 1].offset + specialOff, SEEK_SET) != lines[choice - 1].offset + specialOff) {
             perror("Not correct number of bytes.");
             return -1;
-        };
-        
-        if (read(fd, lineOut, lenStr - specialOff) == -1) {
+        }
+
+        if (read(fd, lineOut, lenStr - specialOff) != lenStr - specialOff) {
             perror("Cannot read line.");
             return -1;
-        };
+        }
         
-        lineOut[lenStr - specialOff] = '\0';
-        printf("%s", lineOut);
+        if (fwrite(strOut, 1, lenStr - specialOff, stdout) != lenStr - specialOff){
+            perror("Cannot write line.");
+            return -1;
+        }
     }
-    
+
     return 1;
 }
 
 int main(int argc, char *argv[]) {
-    
     if (argc != 2) {
-        printf("No filename.\n");
+        fprintf(stderr, "No filename.\n");
         return -1;
     }
 
@@ -82,29 +85,29 @@ int main(int argc, char *argv[]) {
         perror("Opening failed.");
         return -1;
     }
-    
+
     off_t lenStr = 0,
         previousLen = 0, 
         current = 0;
-        
-    int extension = 1, numberOfLine = 0, choice;  
-    
+
+    int extension = 1, numberOfLine = 0, choice = 1;  
+
     char buf[BUFSIZ];
-    
+
     line *lines;
     if (initLine(&lines) == -1){
         return -1;
-    };
-    
+    }
+
     size_t amountBytes;
-    
+
     amountBytes = read(fd, buf, BUFSIZ);
-     
+
     while (amountBytes > 0){
         if (buf[current] == '\n'){
             if (addMemory(&extension, numberOfLine, &lines) == -1){
                 return -1;
-            };
+            }
             lines[numberOfLine].offset = previousLen;
             lines[numberOfLine].len = lenStr + 1;
             previousLen += lenStr + 1;
@@ -122,22 +125,22 @@ int main(int argc, char *argv[]) {
             if (amountBytes == 0 && lenStr > 0){
                 if (addMemory(&extension, numberOfLine, &lines) == -1){
                     return -1;
-                };
+                }
                 lines[numberOfLine].offset = previousLen;
-                lines[numberOfLine].len = lenStr + 1;
+                lines[numberOfLine].len = lenStr;
                 
                 numberOfLine++;
                 break;
             }
             current = 0;
         }
-        
+
     }
-    
-    do {
+
+    while (choice != 0) {
         scanf("%d", &choice);
         while (choice < 0 || choice > numberOfLine) {
-            printf("Incorrect number.\nTry again: ");
+            fprintf(stderr, "Incorrect number.\nTry again: ");
             scanf("%d", &choice);
         }
         if (choice == 0) {
@@ -145,12 +148,12 @@ int main(int argc, char *argv[]) {
         } else {
             if (printLine(lines, fd, choice) == -1){
                 return -1;
-            };
+            }
         }
-    } while (choice != 0);
+    }
 
     free(lines);
     close(fd);
-    
+
     return 0;
 }
